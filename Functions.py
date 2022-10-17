7b0a928a45608c1df0b27c45dd5956ae8e1797d5
Santiago Franco <ktns0930@gmail.com>
2022-10-17 17:06:03 -0500

    Agregadas funcionalidades para extraer la media y la variable aleatoria.

--- Functions.py
+++ Functions.py
@@ -1,3 +1,16 @@
-from Data import database;
-def half():
-    return ''
+from Data import database
+
+def randomVariableExtractor(database, nameOfVariable):
+    extractedVariableArray = []
+    for i in range(0, len(database)):
+        extractedVariableArray.append(database[i].get(nameOfVariable))
+    return extractedVariableArray
+
+def half(randomVariableArray):
+    sum = 0
+    for i in range(0, len(randomVariableArray)):
+        sum += randomVariableArray[i]
+    print(f"La media de la variable es: {round(sum/len(randomVariableArray), 4)}")
+
+arrayOfVariable = randomVariableExtractor(database, "lastSemesterAvg")
+half(arrayOfVariable)
