69b05525bc9925b1bb80a6dcb29069e8d1fafe05
Santiago Franco <ktns0930@gmail.com>
2022-10-17 20:03:36 -0500

    Desviacón estandar lista ppara implementaciÃón.

--- Functions.py
+++ Functions.py
@@ -12,17 +12,25 @@ def half(randomVariableArray):
     sum = 0
     for i in range(0, len(randomVariableArray)):
         sum += randomVariableArray[i]
-    print(f"\nLa media de la variable es: {round(sum/len(randomVariableArray), 4)}")
+    return round(sum/len(randomVariableArray), 4)
 
 def medium(randomVariableArray):
     mediumIndex = math.ceil(len(randomVariableArray)/2)
     mediumValue = randomVariableArray[mediumIndex-1]
-    print(f'\nLa mediana se encuentra en el indice: {mediumIndex} y su valor es: {mediumValue}')
+    return mediumValue, mediumIndex
+def standardDevitation(randomVariableArray):
+    calculatedHalf = half(randomVariableArray)
+    sumOfDistance = 0
+    for i in randomVariableArray:
+        sumOfDistance += (i - calculatedHalf)**2
+    return round(math.sqrt(sumOfDistance/len(randomVariableArray)), 4)
 
-arrayOfVariable = randomVariableExtractor(database, "lastSemesterAvg")
+#arrayOfVariable = randomVariableExtractor(database, "lastSemesterAvg")
+arrayOfVariable = [1, 2.45, 4.5, 5]
 print(arrayOfVariable)
 
-half(arrayOfVariable)
+print(f"\nLa media de la variable es: {half(arrayOfVariable)}")
 
-medium(arrayOfVariable)
+print(f'\nLa mediana se encuentra en el indice: {medium(arrayOfVariable)[1]} y su valor es: {medium(arrayOfVariable)[0]}')
 
+print(f'\nDesviacion estandar: {standardDevitation(arrayOfVariable)}')
