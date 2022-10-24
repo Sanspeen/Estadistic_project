8e60964bf284a25045d2dbe3e6ff1f02f9fadc5f
Santiago Franco <ktns0930@gmail.com>
2022-10-24 16:54:31 -0500

    Refactorizacion de variables y funciones y agregada funcion para separar los datos de los usuarios que trabajan y los que no.

--- Functions.py
+++ Functions.py
@@ -1,34 +1,50 @@
 import math
+import statistics as sta
+from numpy import mean
 from Data import database
 
-def randomVariableExtractor(database, nameOfVariable):
-    extractedVariableArray = []
-    for i in range(0, len(database)):
-        extractedVariableArray.append(database[i].get(nameOfVariable))
-    extractedVariableArray.sort()
-    return extractedVariableArray
 
-def half(randomVariableArray):
-    sum = 0
-    for i in range(0, len(randomVariableArray)):
-        sum += randomVariableArray[i]
-    halfResult = round(sum/len(randomVariableArray), 4)
-    return halfResult, f'La conclusión para la media aritmetica que obtuvimos es que el promedio\n' \
-                                                   f'de nuestra variable tomada fue: {halfResult}'
+def random_variable_extractor(data_base, name_of_variable):
+    extracted_variable_array = []
+    for i in range(0, len(data_base)):
+        extracted_variable_array.append(data_base[i].get(name_of_variable))
+    extracted_variable_array.sort()
+    return extracted_variable_array
 
-def medium(randomVariableArray):
-    mediumIndex = math.ceil(len(randomVariableArray)/2)
-    mediumValue = randomVariableArray[mediumIndex-1]
-    return mediumValue, mediumIndex, f'Nuestro punto medio de datos se encuentra en el indice: {mediumIndex} y toma el valor de: {mediumValue}.'
-def standardDevitation(randomVariableArray):
-    calculatedHalf = half(randomVariableArray)
-    sumOfDistance = 0
-    for i in randomVariableArray:
-        sumOfDistance += (i - calculatedHalf[0])**2
-    return round(math.sqrt(sumOfDistance/len(randomVariableArray)), 4)\
-        ,'La desviacion estandar es la que nos muestra la dispercion entre nuestros datos, a mayor el numero presentado, mayor dispercion entre los datos tendremos...\n' \
-         f'por lo tanto nuestra desviacion estandar presenta un valor de: {round(math.sqrt(sumOfDistance/len(randomVariableArray)), 4)}, Sí fuese 0 indicaria que no hay\n' \
+
+def half(random_variable_array):
+    half_result = round(mean(random_variable_array))
+    return half_result, f'La conclusión para la media aritmetica que obtuvimos es que el promedio\n' \
+                                                   f'de nuestra variable tomada fue: {half_result}'
+
+
+def medium(random_variable_array):
+    medium_index = math.ceil(len(random_variable_array) / 2)
+    medium_value = random_variable_array[medium_index - 1]
+    return medium_value, medium_index, f'Nuestro punto medio de datos se encuentra en el indice: {medium_index} y toma el valor de: {medium_value}.'
+
+
+def standard_devitation(random_variable_array):
+    return round(sta.pstdev(random_variable_array), 4), 'La desviacion estandar es la que nos muestra la dispercion entre nuestros datos, a mayor el numero presentado, mayor dispercion entre los datos tendremos...\n' \
+         f'por lo tanto nuestra desviacion estandar presenta un valor de: {round(sta.pstdev(random_variable_array), 4)}, Sí fuese 0 indicaria que no hay\n' \
          f'ninguna disperción.'
+
+
+def tipic_devitation(random_variable_array):
+    return round(math.sqrt(standard_devitation(random_variable_array)))
+
+
+def is_working_separator(random_variable_array):
+    is_working_users = []
+    not_working_users = []
+    for i in random_variable_array:
+        if(i.get('isWorking') == True):
+            is_working_users.append(i)
+        else:
+            not_working_users.append(i)
+
+    return is_working_users, not_working_users
+
 
 """arrayOfVariable = randomVariableExtractor(database, "lastSemesterAvg")
 #arrayOfVariable = [1, 2.45, 4.5, 5]
--- menu_point_1.py
+++ menu_point_1.py
@@ -2,11 +2,11 @@ import os
 
 from Functions import *
 from Data import database
-def point_A(var1, var2):
+def point_a(var1, var2):
     os.system('cls')
     print(f'Resultado de la variable "{var1}":')
-    var1_array = randomVariableExtractor(database, var1)
-    var2_array = randomVariableExtractor(database, var2)
+    var1_array = random_variable_extractor(database, var1)
+    var2_array = random_variable_extractor(database, var2)
 
     print(f'Datos utilizados:{var2_array}\n')
     print(f"\nLa media de la variable es: {half(var1_array)[0]}.")
@@ -15,8 +15,8 @@ def point_A(var1, var2):
     print(f'\nLa mediana se encuentra en el indice: {medium(var1_array)[1]} y su valor es: {medium(var1_array)[0]}.')
     print(medium(var1_array)[2])
 
-    print(f'\nDesviacion estandar: {standardDevitation(var1_array)[0]}')
-    print(standardDevitation(var1_array)[1])
+    print(f'\nDesviacion estandar: {standard_devitation(var1_array)[0]}')
+    print(standard_devitation(var1_array)[1])
 
     input(f'\nPara ver el resultado de la variable: "{var2}" presione <<ENTER>>.')
 
@@ -28,5 +28,8 @@ def point_A(var1, var2):
     print(f'\nLa mediana se encuentra en el indice: {medium(var2_array)[1]} y su valor es: {medium(var2_array)[0]}.')
     print(medium(var2_array)[2])
 
-    print(f'\nDesviacion estandar: {standardDevitation(var2_array)[0]}')
-    print(standardDevitation(var2_array)[1])
+    print(f'\nDesviacion estandar: {standard_devitation(var2_array)[0]}')
+    print(standard_devitation(var2_array)[1])
+
+def point_b():
+    print('Aqui ira el punto b')
