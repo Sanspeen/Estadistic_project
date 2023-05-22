5d6fefa4c40a6cef63f5d06eba5105e811e331ce
Santiago Franco <ktns0930@gmail.com>
2023-05-22 03:31:41 -0500

    Lista la app :)

--- view.py
+++ view.py
@@ -385,10 +385,10 @@ def open_page_3_indx_e():
 def open_page_4_indx_4():
     new_window = tk.Toplevel(app)
     new_window.title("Punto 4")
-    new_window.geometry("600x600")
+    new_window.geometry("1000x600")
     new_window.configure(bg='#7587c6')
 
-    label = tk.Label(new_window, text=f"Esta es la página", font=("Arial", 14), bg='#7587c6', fg='#ffffff')
+    label = tk.Label(new_window, text=f"Link para ir a la IA:\n https://colab.research.google.com/drive/14Ysfxzw0tE2xZqvCkK2qebIViTKVCgM6?authuser=2#scrollTo=QuW8GFCDCfOS", font=("Arial", 14), bg='#7587c6', fg='#ffffff')
     label.pack(pady=30)
 
     back_button = tk.Button(new_window, text="Volver", command=new_window.destroy, font=("Arial", 12), bg='#465ca9',
@@ -435,9 +435,9 @@ title_label = tk.Label(main_frame, text="Proyecto estadística", font=("Arial", 
 title_label.pack(pady=20)
 
 # Ventana 1
-window1 = create_button_frame(app, "Esta es la ventana 1")
+window1 = create_button_frame(app, "Punto 1")
 window1.place(x=0, y=0, width=600, height=500)
-button1 = tk.Button(main_frame, text="Ventana 1", command=lambda: show_frame(window1), font=("Arial", 12), bg='#4d4d4d',
+button1 = tk.Button(main_frame, text="Punto 1", command=lambda: show_frame(window1), font=("Arial", 12), bg='#4d4d4d',
                    fg='#ffffff', padx=20, pady=5)
 button1.pack(fill=tk.X, padx=50, pady=10)
 
@@ -471,9 +471,9 @@ btn_7 = tk.Button(window1, text=f"Punto g", font=("Arial", 12), bg='#4d4d4d', fg
 btn_7.pack(pady=5)
 
 # Ventana 2
-window2 = create_button_frame(app, "Esta es la ventana 2")
+window2 = create_button_frame(app, "Punto 2")
 window2.place(x=0, y=0, width=600, height=500)
-button2 = tk.Button(main_frame, text="Ventana 2", command=lambda: show_frame(window2), font=("Arial", 12), bg='#4d4d4d',
+button2 = tk.Button(main_frame, text="Punto 2", command=lambda: show_frame(window2), font=("Arial", 12), bg='#4d4d4d',
                    fg='#ffffff', padx=20, pady=5)
 button2.pack(fill=tk.X, padx=50, pady=10)
 
@@ -508,9 +508,9 @@ btn_7 = tk.Button(window2, text=f"Punto g", font=("Arial", 12), bg='#4d4d4d', fg
 btn_7.pack(pady=5)
 
 # Ventana 3
-window3 = create_button_frame(app, "Esta es la ventana 3")
+window3 = create_button_frame(app, "Punto 3")
 window3.place(x=0, y=0, width=600, height=500)
-button3 = tk.Button(main_frame, text="Ventana 3", command=lambda: show_frame(window3), font=("Arial", 12), bg='#4d4d4d',
+button3 = tk.Button(main_frame, text="Punto 3", command=lambda: show_frame(window3), font=("Arial", 12), bg='#4d4d4d',
                    fg='#ffffff', padx=20, pady=5)
 button3.pack(fill=tk.X, padx=50, pady=10)
 
@@ -537,9 +537,9 @@ btn_5 = tk.Button(window3, text=f"Punto e", font=("Arial", 12), bg='#4d4d4d', fg
 btn_5.pack(pady=5)
 
 # Ventana 4
-window4 = create_button_frame(app, "Esta es la ventana 4")
+window4 = create_button_frame(app, "Punto 4")
 window4.place(x=0, y=0, width=600, height=500)
-button4 = tk.Button(main_frame, text="Ventana 4", command=lambda: show_frame(window4), font=("Arial", 12), bg='#4d4d4d',
+button4 = tk.Button(main_frame, text="Punto 4", command=lambda: show_frame(window4), font=("Arial", 12), bg='#4d4d4d',
                    fg='#ffffff', padx=20, pady=5)
 button4.pack(fill=tk.X, padx=50, pady=10)
 
@@ -549,5 +549,7 @@ btn_1 = tk.Button(window4, text=f"Punto 4:", font=("Arial", 12), bg='#4d4d4d', f
                   command=lambda p=i: open_page_4_indx_4())
 btn_1.pack(pady=5)
 
+label_lindos = tk.Label(main_frame, text="By: Julian Isaza G & Santiago Franco M", font=("Arial", 14), bg='#7587c6', fg='#ffffff')
+label_lindos.pack(pady=10)
 show_frame(main_frame)
 app.mainloop()
